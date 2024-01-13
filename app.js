b70db33f050d313e5eee64381fbc64d78d78d26c
Đỗ Tất Hiếu <dotathieu0729@gmail.com>
2024-01-13 22:09:37 +0700

    Day 5: Working on the CLient and Special, unoptimized JS code

--- app.js
+++ app.js
@@ -23,3 +23,89 @@ $$(".js-toggle").forEach((button) => {
         }
     };
 });
+
+// Làm phần lướt của Special
+const arrowLeft = $(".arrow-left");
+const arrowRight = $(".arrow-right");
+const dotsElement = $(".special__dot");
+let totalClick = 0;
+
+if (totalClick === 0) {
+    arrowLeft.classList.add("hide");
+    arrowLeft.classList.remove("show");
+
+    arrowRight.classList.add("show");
+    arrowRight.classList.remove("hide");
+}
+
+arrowRight.onclick = () => {
+    totalClick++;
+    if (totalClick < 0) totalClick = 0;
+    if (totalClick > 2) totalClick = 2;
+
+    if (totalClick === 0) {
+        arrowLeft.classList.add("hide");
+        arrowLeft.classList.remove("show");
+    }
+
+    if (totalClick === 2) {
+        arrowRight.classList.add("hide");
+        arrowRight.classList.remove("show");
+    }
+
+    if (totalClick === 1) {
+        arrowLeft.classList.add("show");
+        arrowLeft.classList.remove("hide");
+
+        arrowRight.classList.add("show");
+        arrowRight.classList.remove("hide");
+    }
+
+    $$(".special-item").forEach((item) => {
+        const translateNum = 100 * totalClick;
+        item.style.translate = `calc(-${translateNum}% - ${30 * totalClick}px)`;
+    });
+
+    const dots = dotsElement.querySelectorAll(".dot");
+    dots.forEach((dot, index) => {
+        dot.classList.remove("active");
+
+        dots[totalClick].classList.add("active");
+    });
+};
+
+arrowLeft.onclick = () => {
+    totalClick--;
+    if (totalClick < 0) totalClick = 0;
+    if (totalClick > 2) totalClick = 2;
+
+    if (totalClick === 0) {
+        arrowLeft.classList.add("hide");
+        arrowLeft.classList.remove("show");
+    }
+
+    if (totalClick === 2) {
+        arrowRight.classList.add("hide");
+        arrowRight.classList.remove("show");
+    }
+
+    if (totalClick === 1) {
+        arrowLeft.classList.add("show");
+        arrowLeft.classList.remove("hide");
+
+        arrowRight.classList.add("show");
+        arrowRight.classList.remove("hide");
+    }
+
+    $$(".special-item").forEach((item) => {
+        const translateNum = 100 * totalClick;
+        item.style.translate = `calc(-${translateNum}% - ${30 * totalClick}px)`;
+    });
+
+    const dots = dotsElement.querySelectorAll(".dot");
+    dots.forEach((dot, index) => {
+        dot.classList.remove("active");
+
+        dots[totalClick].classList.add("active");
+    });
+};
