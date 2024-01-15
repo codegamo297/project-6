b65a3844fbdcba45302dadaf9a4a0044927bb529
Đỗ Tất Hiếu <dotathieu0729@gmail.com>
2024-01-15 23:35:31 +0700

    Day 6: Finished the Project and About, unoptimized JS code (optimized code part Special)

--- app.js
+++ app.js
@@ -28,84 +28,80 @@ $$(".js-toggle").forEach((button) => {
 const arrowLeft = $(".arrow-left");
 const arrowRight = $(".arrow-right");
 const dotsElement = $(".special__dot");
+const dots = dotsElement.querySelectorAll(".dot");
 let totalClick = 0;
 
-if (totalClick === 0) {
-    arrowLeft.classList.add("hide");
-    arrowLeft.classList.remove("show");
+checkClick(totalClick);
 
-    arrowRight.classList.add("show");
-    arrowRight.classList.remove("hide");
+function checkClick(total, lengthList) {
+    total = Math.min(Math.max(total, 0), lengthList);
+    if (!lengthList) total = 0;
+
+    arrowLeft.classList.toggle("hide", total === 0);
+    arrowLeft.classList.toggle("show", total !== 0);
+
+    arrowRight.classList.toggle("hide", total === lengthList);
+    arrowRight.classList.toggle("show", total !== lengthList);
+
+    return total;
 }
 
 arrowRight.onclick = () => {
     totalClick++;
-    if (totalClick < 0) totalClick = 0;
-    if (totalClick > 2) totalClick = 2;
-
-    if (totalClick === 0) {
-        arrowLeft.classList.add("hide");
-        arrowLeft.classList.remove("show");
-    }
-
-    if (totalClick === 2) {
-        arrowRight.classList.add("hide");
-        arrowRight.classList.remove("show");
-    }
-
-    if (totalClick === 1) {
-        arrowLeft.classList.add("show");
-        arrowLeft.classList.remove("hide");
-
-        arrowRight.classList.add("show");
-        arrowRight.classList.remove("hide");
-    }
+    totalClick = checkClick(totalClick, $$(".special-item").length - 1);
 
     $$(".special-item").forEach((item) => {
-        const translateNum = 100 * totalClick;
-        item.style.translate = `calc(-${translateNum}% - ${30 * totalClick}px)`;
+        const translateCss = `calc(-${100 * totalClick}%
+            - ${30 * totalClick}px)`;
+        item.style.translate = translateCss;
     });
 
-    const dots = dotsElement.querySelectorAll(".dot");
-    dots.forEach((dot, index) => {
+    dots.forEach((dot) => {
         dot.classList.remove("active");
-
         dots[totalClick].classList.add("active");
     });
 };
 
 arrowLeft.onclick = () => {
     totalClick--;
-    if (totalClick < 0) totalClick = 0;
-    if (totalClick > 2) totalClick = 2;
-
-    if (totalClick === 0) {
-        arrowLeft.classList.add("hide");
-        arrowLeft.classList.remove("show");
-    }
-
-    if (totalClick === 2) {
-        arrowRight.classList.add("hide");
-        arrowRight.classList.remove("show");
-    }
-
-    if (totalClick === 1) {
-        arrowLeft.classList.add("show");
-        arrowLeft.classList.remove("hide");
-
-        arrowRight.classList.add("show");
-        arrowRight.classList.remove("hide");
-    }
+    totalClick = checkClick(totalClick, $$(".special-item").length - 1);
 
     $$(".special-item").forEach((item) => {
-        const translateNum = 100 * totalClick;
-        item.style.translate = `calc(-${translateNum}% - ${30 * totalClick}px)`;
+        const translateCss = `calc(-${100 * totalClick}%
+            - ${30 * totalClick}px)`;
+        item.style.translate = translateCss;
     });
 
-    const dots = dotsElement.querySelectorAll(".dot");
-    dots.forEach((dot, index) => {
+    dots.forEach((dot) => {
         dot.classList.remove("active");
-
         dots[totalClick].classList.add("active");
     });
 };
+
+// Làm phần About: play video
+const playVideo = $(".video-play__btn");
+const btnVideo = $(".video__btn");
+const video = $(".video__main");
+
+playVideo.onclick = () => {
+    video.play();
+    video.setAttribute("controls", "true");
+    playVideo.style.display = "none";
+    btnVideo.style.display = "none";
+};
+
+video.addEventListener("mousemove", function (event) {
+    var rect = video.getBoundingClientRect();
+    var mouseX = event.clientX - rect.left;
+    var mouseY = event.clientY - rect.top;
+
+    if (
+        mouseX >= 0 &&
+        mouseX <= rect.width &&
+        mouseY >= 0 &&
+        mouseY <= rect.height
+    ) {
+        // console.log("Con trỏ đi qua phần tử.");
+        // Thực hiện các hành động khác khi con trỏ đi qua phần tử.
+    }
+});
